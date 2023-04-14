0149aac7d42efc77941e56a8ecc3bd25d76d3faa
David Avery <avery.di@gmail.com>
2023-04-14 17:48:01 +0000

    Playing Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,60 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function cardPicker() {
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["Spades", "Clubs", "Diamonds", "Hearts"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  const cards = [];
+  for (let st = 0; st < suits.length; st++) {
+    for (let nb = 0; nb < numbers.length; nb++) {
+      const number = numbers[nb];
+      const suit = suits[st];
+      cards.push({ number, suit });
+    }
+  }
+  return cards;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const cards = cardPicker();
+randomCard(cards);
+
+function randomCard(cards) {
+  const random = Math.floor(Math.random() * 51);
+  const cardNumber = cards[random].number;
+  const cardSuit = cards[random].suit;
+  let character;
+  cardSuit === "Diamonds"
+    ? (character = "&diams;")
+    : (character = "&" + cardSuit.toLowerCase() + ";");
+  const card = document.createElement("div");
+  card.classList.add("card", cardSuit.toLowerCase());
+  card.innerHTML =
+    '<span class="card-character-suit top">' +
+    cardNumber +
+    character +
+    "</span>" +
+    '<span class="card-character">' +
+    cardNumber +
+    "</span>" +
+    '<span class="card-character-suit bot">' +
+    cardNumber +
+    character +
+    "</span>";
+  document.body.appendChild(card);
+}
